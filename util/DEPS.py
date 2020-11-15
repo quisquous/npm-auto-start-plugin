955eddcc3f3dbccb0ce22999144b305385f600c9
Adrienne Walker <enne@quisquo.us>
2020-11-15 15:47:43 -0800

    add deps fetching

---
+++ util/DEPS.py
@@ -0,0 +1,14 @@
+deps = {
+    "ACT": {
+        "url": "https://github.com/EQAditu/AdvancedCombatTracker/releases/download/3.4.6.267/ACTv3.zip",
+        "dest": "plugin/ThirdParty/ACT",
+        "strip": 0,
+        "hash": ["sha256", "adf13a38d0938ce90f8e674f8365b227d933b91636ddf72b26c85702f6e3b808"],
+    },
+    "OverlayPlugin": {
+        "url": "https://github.com/ngld/OverlayPlugin/releases/download/v0.16.2/OverlayPlugin-0.16.2.zip",
+        "dest": "plugin/ThirdParty/OverlayPlugin",
+        "strip": 1,
+        "hash": ["sha256", "adf13a38d0938ce90f8e674f8365b227d933b91636ddf72b26c85702f6e3b808"],
+    },
+}
