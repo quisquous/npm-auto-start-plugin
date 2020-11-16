f347620d94d34ed0fa34b3f8d033300050c2b115
Adrienne Walker <enne@quisquo.us>
2020-11-15 16:22:41 -0800

    initial plugin

---
+++ plugin/NpmAutoStartPlugin/OverlayAddonMain.cs
@@ -0,0 +1,17 @@
+﻿using RainbowMage.OverlayPlugin;
+
+namespace NpmAutoStart {
+  public class OverlayAddonMain : IOverlayAddonV2 {
+    public string Name {
+      get { return "NpmAutoStart"; }
+    }
+
+    public string Description {
+      get { return "Automatically runs `npm start`"; }
+    }
+
+    public void Init()
+    {
+    }
+  }
+}
---
+++ plugin/NpmAutoStartPlugin/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NpmAutoStart")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NpmAutoStart")]
+[assembly: AssemblyCopyright("Copyright 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("7a65ef97-fa62-4cee-a199-4e9be0e1a9c4")]
+
+// Version:
+// - Major Version
+// - Minor Version
+// - Build Number
+// - Revision
+// GitHub has only 3 version components, so Revision should always be 0.
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
